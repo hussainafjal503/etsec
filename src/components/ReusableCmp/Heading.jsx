aaa3511f5a5753c3c75b73ca7cdd0e7c00e10ae9
Afjal hussain <mdafjalhussain503@gmail.com>
2025-03-09 20:50:43 +0530

    heading bug fixed

--- src/components/ReusableCmp/Heading.jsx
+++ src/components/ReusableCmp/Heading.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function Heading({children}) {
+function Heading({ children }) {
   return (
-	<div className='flex flex-row gap-2 items-center '>
-		<div className='flex flex-col justify-center gap-2'>
-		<div className={`border-b-2 border-[#06BBCC]  w-10 md:max-w-20  ml-auto`}></div>
-		<div className='border-b-2 border-[#06BBCC]  w-15 md:max-w-30'></div>
+    <div className="flex flex-row gap-2 items-center ">
+      <div className="flex flex-col justify-center gap-2">
+        <div
+          className={`border-b-2 border-[#06BBCC]  w-8 md:max-w-20  ml-auto`}
+        ></div>
+        <div className="border-b-2 border-[#06BBCC]  w-12 md:max-w-30"></div>
+      </div>
+      <div className="uppercase text-[#06BBCC] text-lg md:text-2xl font-bold ">
+        {children}
+      </div>
 
-		</div>
-		<div className='uppercase text-[#06BBCC] text-xl md:text-2xl font-bold '>{children}</div>
-
-		<div className='flex flex-col justify-center gap-2'>
-		<div className={`border-b-2 border-[#06BBCC]  w-10 md:max-w-20 `}></div>
-		<div className='border-b-2 border-[#06BBCC]  w-15 md:max-w-30'></div>
-
-		</div>
-	  
-	</div>
-  )
+      <div className="flex flex-col justify-center gap-2">
+        <div className={`border-b-2 border-[#06BBCC]  w-8 md:max-w-20 `}></div>
+        <div className="border-b-2 border-[#06BBCC]  w-12 md:max-w-30"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Heading
+export default Heading;
