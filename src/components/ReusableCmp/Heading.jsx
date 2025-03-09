d6f6653b515c3a479d2db94053523cde41beb1d7
Afjal hussain <mdafjalhussain503@gmail.com>
2025-03-09 20:41:28 +0530

    making nav fully functional with auto scrolling features

--- src/components/ReusableCmp/Heading.jsx
+++ src/components/ReusableCmp/Heading.jsx
@@ -4,15 +4,15 @@ function Heading({children}) {
   return (
 	<div className='flex flex-row gap-2 items-center '>
 		<div className='flex flex-col justify-center gap-2'>
-		<div className={`border-b-2 border-[#06BBCC] w-2 sm:w-5 md:w-20  ml-auto`}></div>
-		<div className='border-b-2 border-[#06BBCC] w-4 sm:w-15 md:w-30'></div>
+		<div className={`border-b-2 border-[#06BBCC]  w-10 md:max-w-20  ml-auto`}></div>
+		<div className='border-b-2 border-[#06BBCC]  w-15 md:max-w-30'></div>
 
 		</div>
-		<div className='uppercase text-[#06BBCC] text-sm md:text-2xl font-bold '>{children}</div>
+		<div className='uppercase text-[#06BBCC] text-xl md:text-2xl font-bold '>{children}</div>
 
 		<div className='flex flex-col justify-center gap-2'>
-		<div className={`border-b-2 border-[#06BBCC] w-2 sm:w-10 md:w-20 `}></div>
-		<div className='border-b-2 border-[#06BBCC] w-4 sm:w-15 md:w-30'></div>
+		<div className={`border-b-2 border-[#06BBCC]  w-10 md:max-w-20 `}></div>
+		<div className='border-b-2 border-[#06BBCC]  w-15 md:max-w-30'></div>
 
 		</div>
 	  
